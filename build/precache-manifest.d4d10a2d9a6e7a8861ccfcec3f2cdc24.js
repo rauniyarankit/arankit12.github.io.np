cd1a405b717bc92fbe4177619424d329fb540c82
Ankit Rauniyar <rauniyar2012@gmail.com>
2021-11-25 19:45:23 +1100

    Updates

---
+++ build/precache-manifest.d4d10a2d9a6e7a8861ccfcec3f2cdc24.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb0bbfdfa3747cae3fc62ee766e1d620",
+    "url": "/index.html"
+  },
+  {
+    "revision": "921ac5c99786efb42e13",
+    "url": "/static/css/main.aea59a1d.chunk.css"
+  },
+  {
+    "revision": "2510ea090e29edf31c74",
+    "url": "/static/js/2.7d6d0925.chunk.js"
+  },
+  {
+    "revision": "c87e50d81cc7b5311525cc6fd5482ea5",
+    "url": "/static/js/2.7d6d0925.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "921ac5c99786efb42e13",
+    "url": "/static/js/main.280163ee.chunk.js"
+  },
+  {
+    "revision": "e034efadbb651f000ea6",
+    "url": "/static/js/runtime-main.deb660d4.js"
+  },
+  {
+    "revision": "42e1233fb2ee9492d91fd7495e6559f0",
+    "url": "/static/media/ankit.42e1233f.jpg"
+  },
+  {
+    "revision": "633ff0578bf7ce98b7a618bf6bea742f",
+    "url": "/static/media/aws-ccp.633ff057.png"
+  },
+  {
+    "revision": "68510a7acc412de9623d9c61a4d81f26",
+    "url": "/static/media/aws-csaa.68510a7a.png"
+  },
+  {
+    "revision": "335b251b7470d77f13951af1d30f16e6",
+    "url": "/static/media/ccent.335b251b.png"
+  },
+  {
+    "revision": "76a868d8e7de8ee392a80c93c41d9527",
+    "url": "/static/media/ccna.76a868d8.png"
+  },
+  {
+    "revision": "b93c8ca1c66f30be3ee3ca76ab45aa23",
+    "url": "/static/media/mcaf.b93c8ca1.png"
+  },
+  {
+    "revision": "bd928b03c7c460de862f4e12bd3b9cef",
+    "url": "/static/media/mcaif.bd928b03.png"
+  }
+]);
